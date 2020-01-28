39610b86f6a341e17e4a0e9096846c530deae6ea
dimparato <ponticremator@gmail.com>
2020-01-27 20:56:25 -0500

    seeds fixed

--- seeds.sql
+++ seeds.sql
@@ -2,37 +2,34 @@ DROP DATABASE IF EXISTS speakUp_db;
 CREATE DATABASE speakUp_db;
 USE speakUp_db;
 
---Creating "users" table--
 CREATE TABLE users (
-  userID INTEGER NOT NULL AUTO INCREMENT,
+  userID INTEGER NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
   password VARCHAR(30) NOT NULL,
   admin BOOLEAN NOT NULL,
-  dateJoined DATETIME DEFAULT GETDATE(),
+  dateJoined DATETIME,
   postNumber INTEGER(10),
   PRIMARY KEY (userID)
 );
 
---Creating "forums" table--
 CREATE TABLE forums (
-  forumID INTEGER NOT NULL AUTO INCREMENT,
+  forumID INTEGER NOT NULL AUTO_INCREMENT,
   title VARCHAR(50) NOT NULL,
-  description SMALLTEXT,
+  description TEXT,
   userID INTEGER NOT NULL,
-  dateCreated DATETIME DEFAULT GETDATE(),
+  dateCreated DATETIME,
   PRIMARY KEY (forumID),
   FOREIGN KEY (userID) REFERENCES users(userID)
 );
 
---Creating "posts" table--
 CREATE TABLE posts (
-  postID INTEGER NOT NULL AUTO INCREMENT,
+  postID INTEGER NOT NULL AUTO_INCREMENT,
   forumID INTEGER NOT NULL,
   subject VARCHAR(100) NOT NULL,
   content MEDIUMTEXT,
   userID INTEGER NOT NULL,
-  datePosted DATETIME DEFAULT GETDATE(),
+  datePosted DATETIME,
   PRIMARY KEY (postID),
-  FOREIGN KEY (forum) REFERENCES forums(forumID),
+  FOREIGN KEY (forumID) REFERENCES forums(forumID),
   FOREIGN KEY (userID) REFERENCES users(userID)
 )
