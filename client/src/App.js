bf280e703860c0efd584e74955ec1e21bc1ba508
Mikirtumov <you@example.com>
2020-01-29 20:46:24 -0500

    sidebar added fixed color

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 //import "./style.scss";
-
+import Sidebar from "./components/Sidebar";
 // import { Login, Register } from "./components/login/index";
 import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
+
 import Nav from "./components/Nav";
 function App() {
   return (
     <Router>
       <div>
+      
         <Nav />
+        <Sidebar/>
         <Switch>
        
         
