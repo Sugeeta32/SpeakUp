e44fdf760158f8b936b68381e0b5abc95a184e02
Sugeeta32 <sugeeta.gupta@gmail.com>
2020-02-02 19:58:44 -0500

    Home

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Forum from './components/forum'
 import Navbar from './components/navbar'
 import SignIn from './pages/SignIn'
 import NoMatch from './pages/NoMatch'
+import Home from './pages/Home'
 //import Home from './components/home'
 
 
@@ -26,20 +27,20 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-<Switch>
-        <Route
-          exact path="/forum"
-          render={() =>
-            <Forum
-              loggedIn={this.state.loggedIn}
-            />}
-
-
-        />
-
-        <Route path="/login" component={SignIn} />
-        <Route path="/signup" component={SignIn} />
-        <Route component={NoMatch} />
+        <Switch>
+          <Route
+            exact path="/forum"
+            render={() =>
+              <Forum
+                loggedIn={this.state.loggedIn}
+              />}
+          />
+          <Route
+            exact path="/"
+            component={Home} />
+          <Route path="/login" component={SignIn} />
+          <Route path="/signup" component={SignIn} />
+          <Route component={NoMatch} />
         </Switch>
       </div>
     );
