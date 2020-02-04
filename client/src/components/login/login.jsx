202cd186900fdbb2d4c6154096fabbfcd38bbc1a
Sugeeta32 <sugeeta.gupta@gmail.com>
2020-02-04 12:21:19 -0500

    login and registration page modification

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,36 +1,89 @@
 import React from "react";
 import logo from "../../logo.png";
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+
+
 export class Login extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+    //set the state to initial value
+    this.state = {
+      username: '',
+      password: '',
+      redirectTo: null
+    }
+    //handle the click of submit button
+    this.handleSubmit = this.handleSubmit.bind(this)
+    //handle the change in username and password
+    this.handleChange = this.handleChange.bind(this)
+  }
+//handlesubmit funtion
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log("handleSubmit")
+    //instructing Axios to send a POST request to /user/login with an object of key/value pairs as its data. Axios will automatically convert the data to JSON and send it as the request body.
+    axios.post("./user/login", {
+      username: this.state.username,
+      password: this.state.password
+
+    }).then(res => {
+      console.log("login response" + res)
+      if (res.status === 200) {
+
+        this.props.updateUser({
+          loggedIn: true,
+          username: res.data.username
+        })
+        this.setState({
+          redirectTo: "/forum"
+        })
+      }
+
+    }).catch(err => {
+      console.log("login error" + err)
+    })
+
+  }
+
+  //handlechange function
+  handleChange(e){
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
+    if(this.state.redirectTo){
+return<Redirect to ={{pathname: this.state.redirectTo}}/>
+    }else{
     return (
       <div className="base-container" ref={this.props.containerRef}>
         <div className="header">Login</div>
         <div className="content">
-        <div className="image">
+          <div className="image">
             <img src={logo} />
           </div>
 
           <div className="form">
             <div className="form-group">
               <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
+              <input type="text" name="username" placeholder="username" value={this.state.username}
+                onChange={this.handleChange} />
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" />
+              <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.handleChange} />
             </div>
           </div>
         </div>
         <div className="footer">
-          <button type="submit" className="btn1">
+          <button type="submit" onClick={this.handleSubmit} className="btn1">
             Login
           </button>
         </div>
       </div>
-    );
+    )
+  }
   }
 }
--- client/src/components/login/register.jsx
+++ client/src/components/login/register.jsx
@@ -1,38 +1,96 @@
 import React from "react";
 import logo from "../../logo.png";
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
 export class Register extends React.Component {
   constructor(props) {
     super(props);
+    //set the state to initial value
+    this.state = {
+      username: '',
+      email: '',
+      password: '',
+      redirectTo: null
+    }
+    //handle the click of submit button
+    this.handleSubmit = this.handleSubmit.bind(this)
+    //handle the change in username and password
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log("register handlesubmit, username : " + this.state.username)
+
+    //request server to add new username,email and paasword
+    axios.post("./user/", {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    }).then(res => {
+      console.log("registration response" + res)
+      if (!res.data.errmsg) {
+
+        console.log("successful registration")
+        this.setState({
+
+          //redirected to login page 
+          redirectTo: "/login"
+        })
+
+      } else {
+        console.log("username not available")
+      }
+
+    }).catch(err => {
+      console.log("registartion error" + err)
+    })
+
+  }
+
+  //handlechange function
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   render() {
-    return (
-      <div className="base-container" ref={this.props.containerRef}>
-        <div className="header">Register</div>
-        <div className="content">
-        
-          <div className="form">
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="email" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="text" name="password" placeholder="password" />
+    if (this.state.redirectTo) {
+      return <Redirect to={{ pathname: this.state.redirectTo }} />
+    } else {
+      return (
+        <div className="base-container" ref={this.props.containerRef}>
+          <div className="header">Register</div>
+          <div className="content">
+
+            <div className="form">
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input type="text" name="username" placeholder="username" value={this.state.username}
+                  onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" placeholder="email" value={this.state.email}
+                  onChange={this.handleChange} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input type="text" name="password" placeholder="password" value={this.state.password}
+                  onChange={this.handleChange} />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="footer">
-          <button type="button" className="btn1">
-            Register
+          <div className="footer">
+            <button type="submit" onClick={this.handleSubmit} className="btn1">
+              Register
           </button>
+          </div>
         </div>
-      </div>
-    );
+      )
+    }
   }
 }
